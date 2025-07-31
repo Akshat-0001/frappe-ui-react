ed726bedd445e68d5747b99fbee39e860874b36b
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2025-07-31 13:43:38 +0530

    feat: Remove unused SVG icons and update index export

--- packages/frappe-components/src/icons/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as ChevronDown } from './chevron-down.svg?react';
-export { default as ChevronRight } from './chevron-right.svg?react';
-export { default as LoadingIndicatorIcon } from './loading-indicator.svg?react';
-export { default as Check } from './check.svg?react';
-export { default as X } from './x.svg?react';
